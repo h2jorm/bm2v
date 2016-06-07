13d65e56bab2951d03bc5e53c03d9cf0568992dd
leeching <417300981@qq.com>
2016-06-07 18:43:30 +0800

    feat(binder): add if binder

--- example/snippets/hello.js
+++ example/snippets/hello.js
@@ -2,15 +2,20 @@
 (function hello() {
   var helloExample = document.getElementById('hello');
   var helloModel = new Model({
-    name: '',
+    name: 'world',
+    show: true,
   });
-  var tmpl = 'name: <input><div>hello, <span data-model="name"></span></div>';
+  var tmpl = 'name: <input><button>toggle</button><div data-message>hello, <span data-model="name"></span></div>';
   var view = new View({
     template: tmpl,
     model: helloModel,
     bind: {
       name: [
         ['text', '[data-model="name"]'],
+        ['form', 'input'],
+      ],
+      show: [
+        ['if', '[data-message]'],
       ],
     },
     events: {
@@ -18,7 +23,10 @@
         var newName = event.currentTarget.value;
         helloModel.update('name', newName);
       }],
-    }
+      'button': ['click', function (event) {
+        helloModel.update('show', !helloModel.get('show'));
+      }],
+    },
   });
   helloExample.appendChild(view.dom);
   pushSnippet(hello);
--- src/binders/index.js
+++ src/binders/index.js
@@ -2,5 +2,6 @@ import './class';
 import './empty';
 import './for';
 import './form';
+import './if';
 import './json';
 import './text';
