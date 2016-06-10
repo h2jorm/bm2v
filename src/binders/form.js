44cbac5e5c39b629706643767e9d43f8eb188fc4
leeching <417300981@qq.com>
2016-06-10 08:47:11 +0800

    feat(binder): add the 3rd parameter in form binder

--- src/binders/form.js
+++ src/binders/form.js
@@ -1,20 +1,29 @@
 import {Binder} from '../core/binder';
 
-Binder.register('form', function (view, selector) {
+Binder.register('form', function (view, selector, transform) {
   const inputs = view.query(selector);
   this.update = function (value) {
+    const ret = transformVal(value);
     inputs.forEach(input => {
       switch (input.type) {
         case 'text':
-        input.value !== value ? input.value = value : '';
+        input.value !== ret ? input.value = ret : '';
+        break;
+        case 'number':
+        input.value !== parseInt(ret) ? input.value = parseInt(ret) : '';
         break;
         case 'checkbox':
-        input.checked = !!value;
+        input.checked = !!ret;
         break;
         case 'radio':
-        input.checked = input.value === value;
+        input.checked = input.value === ret;
         break;
       }
     });
   };
+  function transformVal(value) {
+    if (typeof transform === 'function')
+      return transform(value)
+    return value;
+  }
 });
